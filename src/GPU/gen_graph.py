3842fedd70f8244a6da77ee4ff8af875a51b35e8
michaelgathara <mikegtrm@gmail.com>
2024-04-09 22:26:02 -0500

    a set graph

--- src/GPU/gen_graph.py
+++ src/GPU/gen_graph.py
@@ -1,15 +1,21 @@
 import numpy as np
 import sys
 
+"""
+to run
+
+python3 gen_graph.py <num of edges> <max number for a node val> <how many graphs>
+"""
+
 num_edges = int(sys.argv[1])  # Number of edges in each graph
-max_node_value = 5000  # Maximum node value
+max_node_value = int(sys.argv[2])  # Maximum node value
 np.random.seed(42)  
 
 header = "#pragma once\n#include <vector>\n"
 src_nodes_cpp = header + "std::vector<std::vector<int>> srcNodesVec = {\n"
 dest_nodes_cpp = "std::vector<std::vector<int>> destNodesVec = {\n"
 
-for _ in range(3):  
+for _ in range(int(sys.argv[3])):  
     src_nodes = np.random.randint(1, max_node_value, num_edges)
     dest_nodes = np.random.randint(1, max_node_value, num_edges)
     
